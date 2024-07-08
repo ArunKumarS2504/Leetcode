b83ac6aded8ea11ce70b6f2840037ef3668cee97
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-07-08 22:25:09 +0530

    Codechef

---
+++ Arrays/SearchElement.java
@@ -0,0 +1,28 @@
+
+class SearchElement
+{
+	public static void main (String[] args) 
+	{
+		// your code goes here
+		
+		Scanner s = new Scanner(System.in);
+		int A=s.nextInt();
+		int Value=s.nextInt();
+		
+		boolean found = false;
+		for(int i=0;i<A;i++){
+		    int element=s.nextInt();
+		    if(element== Value){
+		       found=true;
+		    break; 
+		    }
+		}
+		
+		if(found){
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("No");
+		}
+	}
+}
