de5e5effd3c6e5fce8ac95d756a713123ca1de15
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-07-30 21:07:38 +0530

    patren

---
+++ Patterns/star.java
@@ -0,0 +1,12 @@
+public class star {
+    public static void main(String[] args) {
+        int number = 5;
+
+        for(int i=0;i<number;i++){
+            for(int j=0;j<number;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
