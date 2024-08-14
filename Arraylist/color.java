2dc4ad0e9e02b0f9ade8db82adc38a57cb7eb3f6
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-08-14 09:40:52 +0530

    list

---
+++ Arraylist/color.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class color {
+    
+    public static void main(String[] args) {
+        
+        ArrayList<String> color1 = new ArrayList<String>();
+
+        color1.add("Ak");
+     }
+}
