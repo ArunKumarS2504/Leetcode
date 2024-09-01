483b455901212ec68b01f19df1371a524af1e7c8
ArunKumarS2504 <149867644+ArunKumarS2504@users.noreply.github.com>
2024-09-01 21:34:16 +0530

    reverseDigit

---
+++ Arraylist/reverseDigit.java
@@ -0,0 +1,23 @@
+
+class reverseDigit {
+    public int reverse(int x) {
+         int reversed = 0;
+
+        while (x != 0) {
+            int digit = x % 10;  
+            x /= 10;             
+
+            // Check for overflow/underflow before multiplying by 10
+            if (reversed > Integer.MAX_VALUE / 10 || (reversed == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+            }
+            if (reversed < Integer.MIN_VALUE / 10 || (reversed == Integer.MIN_VALUE / 10 && digit < -8)) {
+                return 0;
+            }
+
+            reversed = reversed * 10 + digit;  
+        }
+
+        return reversed;
+    }
+}
